9829763ea9e1c1f9e97bc18a86844cfbffec61a8
Edward Boado <eboado8862@conestogac.on.ca>
2022-08-20 23:59:24 -0400

    Update questions with prompts

--- controllers/module.js
+++ controllers/module.js
@@ -1,9 +1,10 @@
 // Define the questions for savings module
 var savingsQuestions = [   
     {
+      // Question type 0 = fill in blank, 1 = multiple choice
       type: 1,
-      prompt: "",
-      question: "Setting a budget is an important part of saving. This allows you to control and track your spending, while setting a realistic amount to save. Where is the best place to store your cash savings?",
+      prompt: "Setting a budget is an important part of saving. This allows you to control and track your spending, while setting a realistic amount to save.",
+      question: "Where is the best place to store your cash savings?",
       choices: [
         "Under the mattress", 
         "A safe deposit box", 
@@ -13,9 +14,9 @@ var savingsQuestions = [
       correctAnswer: "Savings account"
     }, 
     {
-      type: 0,
-      prompt: "",
-      question: "An online savings account is a savings account that only operates online and therefore tends to offer higher interest rates than regular brick-and-mortar banks, which have higher overhead costs. Which of the following is an online financial institution?",
+      type: 1,
+      prompt: "An online savings account is a savings account that only operates online and therefore tends to offer higher interest rates than regular brick-and-mortar institutions, which have higher overhead costs. ",
+      question: "Which of the following is an online financial institution?",
       choices: [
         "Royal Bank", 
         "Scotiabank", 
@@ -25,32 +26,32 @@ var savingsQuestions = [
       correctAnswer: "Wealthsimple"
     },    
     {
-      type: 0,
-      prompt: "",
-      question: "An online savings account is a savings account that only operates online and therefore tends to offer higher interest rates than regular brick-and-mortar banks, which have higher overhead costs. Which of the following is an online financial institution?",
+      type: 1,
+      prompt: "Different financial institutions' savings accounts have different interest rates: Wealthsimple 1%, RBC 0.05%, CIBC 0.10%. Higher rates mean more money earned in your account.",
+      question: "Why do RBC and CIBC pay so much less interest than Wealthsimple?",
       choices: [
-        "Royal Bank", 
-        "Scotiabank", 
-        "Wealthsimple", 
-        "Bank of Canada"
+        "They are bigger companies", 
+        "Actually, they pay more interest", 
+        "They are brick-and-mortar institutions", 
+        "They don't want to pay more"
       ],
-      correctAnswer: "Wealthsimple"
+      correctAnswer: "They are brick-and-mortar institutions"
     },
     {
       type: 0,
-      prompt: "",
+      prompt: "A Registered Retirement Savings Plan is an account that lets you defer and reduce your taxes for when you retire.",
       question: "Saving money in an RRSP will help you after you _____.",
       choices: [
-        "retire", 
+        "stop working", 
         "graduate", 
-        "lose your job", 
+        "have a child", 
         "get married"
       ],
-      correctAnswer: 0
+      correctAnswer: "stop working"
     },    
     {
       type: 0,
-      prompt: "",
+      prompt: "A Tax-Free Savings Account lets you grow your investments tax free on the gains you make over time.",
       question: "A TFSA is a _____ Savings Account.",
       choices: [
         "Transferable", 
@@ -58,25 +59,25 @@ var savingsQuestions = [
         "Tax-Free", 
         "Tax-Forced"
       ],
-      correctAnswer: 2
+      correctAnswer: "Tax-Free"
     }
 ]
 
 // Define the questions for investing module
 var investmentQuestions = [  
   {
-    id: 0,
+    // Question type 0 = fill in blank, 1 = multiple choice
     type: 1,
-    question: "A good rule of thumb is to save at least 20% of your income each month for emergencies, life events, and retirement. If you start out saving only $50 from your paycheck each month, how much would you have saved after 2 years?",
+    prompt: "Investing involves committing money in order to earn a financial return. This essentially means that you invest money to make money and achieve your financial goals. Financial institutions offer investment products such as ETFs, Stocks, and Mutual Funds.",
+    question: "If you invest money to make money, which of the following is the best investment?",
     choices: [
-      "$300", 
-      "$400", 
-      "$1000", 
-      "$1200"
+      "Mutual Funds", 
+      "Cars", 
+      "Shoes", 
+      "Cash"
     ],
-    correctAnswer: "$1200",
-    module: "savings"
-  }    
+    correctAnswer: "Mutual Funds"
+  }, 
 ]
 
 exports.getQuestions = async (req, res, next) => {
